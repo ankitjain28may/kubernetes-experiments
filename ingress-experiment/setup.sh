5c2009613e10c58d81d11eafb0584daaab2f8a3b
ankitjain28may <ankitjain28may77@gmail.com>
2019-06-21 00:12:46 +0530

    Added multi ingress contr  with basic auth using Traefik

--- ingress-experiment/setup.sh
+++ ingress-experiment/setup.sh
@@ -4,7 +4,7 @@ kubectl apply -f traefik-crb.yaml --validate=false
 kubectl apply -f traefik-config.yaml --validate=false
 kubectl apply -f traefik-deployment.yaml --validate=false
 kubectl apply -f traefik-svc.yaml --validate=false
-kubectl apply -f traefik-ing.yaml --validate=false
+kubectl apply -f traefik-ingress.yaml --validate=false
 kubectl create namespace "testing"
 kubectl apply -f app.yaml --validate=false
-kubectl apply -f ing.yaml --validate=false
+kubectl apply -f ingress.yaml --validate=false
